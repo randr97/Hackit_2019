a261ed2af1841fb8d8307198777c01028f184cdc
DhruvaPatil98 <dhruva.patil@galepartners.com>
2019-09-06 23:12:22 +0530

    [H2-1] Modified views for git

--- server/app/app.py
+++ server/app/app.py
@@ -1,9 +1,9 @@
 # compose_flask/app.py
-from flask import Flask
+import flask
 from redis import Redis
 from dao.git_dao import GitDatabase
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 redis = Redis(host='redis', port=6379)
 
@@ -12,10 +12,10 @@ def hello():
     redis.incr('hits')
     return 'This Compose/Flask demo has been viewed %s time(s).' % redis.get('hits')
 
-@app.route('/gitcommits/')
+@app.route('/gitcommits', methods=["GET"])
 def git_commits():
     git_dao_object = GitDatabase()
-    return git_dao_object.get_all_commits()
+    return flask.jsonify({"result": git_dao_object.get_all_commits()})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
