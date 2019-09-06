4b1ab7074ecc793fa86c0f231736a5032fd199cc
DhruvaPatil98 <dhruva.patil@galepartners.com>
2019-09-07 00:31:24 +0530

    [H2-1] Added getting commits from individual user

--- server/app/job/git_job.py
+++ server/app/job/git_job.py
@@ -5,7 +5,7 @@ from github import Github
 
 from dao.git_dao import GitDatabase
 
-TOKEN = '6d75e9cccad717b4a453b5bf4e945386ef1a7b6d'
+TOKEN = 'ead9543ebd19db345ffadaff493d006201ff305a'
 REPO_NAME = 'Hackit_2019'
 
 class GitHubJob:
@@ -15,7 +15,7 @@ class GitHubJob:
         self.repo = self.get_repo(user_name+'/'+repo_name)
 
 
-    def get_admin(self, token): # Ex: dhruva - 6d75e9cccad717b4a453b5bf4e945386ef1a7b6d, rohit - 80b7b2fbc18b0d6192b19aaffae39aa2058cfc76
+    def get_admin(self, token): # Ex: dhruva - 6d75e9cccad717b4a453b5bf4e945386ef1a7b6d, rohit - ead9543ebd19db345ffadaff493d006201ff305a
         return Github(token)
 
     def get_repo(self, repo_id_or_name): # Ex: "194830160" or "DhruvaPatil98/alchemy-marketing-ui"
@@ -24,44 +24,37 @@ class GitHubJob:
     def get_commits_in_range(self, **kwargs):
         return self.repo.get_commits(**kwargs)
 
-    def get_commit_stats(self, newcommit, jira_ticket="H2-1"):
-        if jira_ticket in newcommit.commit.message:    
-            files = newcommit.files
-            dict_lang = {
-                "additions_python": sum([i.additions for i in files if 'py' in i.filename.split('.')[-1]]),
-                "deletions_python": sum([i.deletions for i in files if 'py' in i.filename.split('.')[-1]]),
-                "additions_java": sum([i.additions for i in files if 'js' in i.filename.split('.')[-1]]),
-                "deletions_java": sum([i.deletions for i in files if 'js' in i.filename.split('.')[-1]]),
-                "additions_html": sum([i.additions for i in files if 'html' in i.filename.split('.')[-1]]),
-                "deletions_html": sum([i.deletions for i in files if 'html' in i.filename.split('.')[-1]]),
-                "additions_css": sum([i.additions for i in files if 'less' in i.filename.split('.')[-1]]),
-                "deletions_css": sum([i.deletions for i in files if 'less' in i.filename.split('.')[-1]]),
-            }
-            stats = {
-                "commit_id": newcommit.sha,
-                "repo_id": self.repo.id,
-                "author_name": newcommit.commit.author.name,
-                "author_id": newcommit.author.id,
-                "commit_message": newcommit.commit.message,
-                "additions": newcommit.stats.additions,
-                "deletetions": newcommit.stats.deletions,
-                "total": newcommit.stats.total,
-                "time": newcommit.commit.author.date.replace(tzinfo=datetime.timezone.utc).astimezone(tz=None),
-            }
-            stats.update(dict_lang)
-            return stats
-        else:
-            return None
+    def get_commit_stats(self, newcommit):
+        files = newcommit.files
+        dict_lang = {
+            "additions_python": sum([i.additions for i in files if 'py' in i.filename.split('.')[-1]]),
+            "deletions_python": sum([i.deletions for i in files if 'py' in i.filename.split('.')[-1]]),
+            "additions_java": sum([i.additions for i in files if 'js' in i.filename.split('.')[-1]]),
+            "deletions_java": sum([i.deletions for i in files if 'js' in i.filename.split('.')[-1]]),
+            "additions_html": sum([i.additions for i in files if 'html' in i.filename.split('.')[-1]]),
+            "deletions_html": sum([i.deletions for i in files if 'html' in i.filename.split('.')[-1]]),
+            "additions_css": sum([i.additions for i in files if 'less' in i.filename.split('.')[-1]]),
+            "deletions_css": sum([i.deletions for i in files if 'less' in i.filename.split('.')[-1]]),
+        }
+        stats = {
+            "commit_id": newcommit.sha,
+            "repo_id": self.repo.id,
+            "author_name": newcommit.commit.author.name,
+            "author_id": newcommit.author.id,
+            "commit_message": newcommit.commit.message,
+            "additions": newcommit.stats.additions,
+            "deletetions": newcommit.stats.deletions,
+            "total": newcommit.stats.total,
+            "time": newcommit.commit.author.date.replace(tzinfo=datetime.timezone.utc).astimezone(tz=None),
+        }
+        stats.update(dict_lang)
+        return stats
 
     def git_job(self):
         try:
             database_object = GitDatabase()
-            all_commits = self.get_commits_in_range(author=self.user_name)
-            all_commits_status = []
-            for c in all_commits:
-                newcommit = self.get_commit_stats(c)
-                if newcommit:
-                    all_commits_status.append(newcommit)
+            all_commits = self.get_commits_in_range()
+            all_commits_status = [self.get_commit_stats(c) for c in all_commits]
             print("*********************")
             print(all_commits_status[-1])
             print("*********************")
@@ -73,6 +66,7 @@ class GitHubJob:
             return False
 
 job_object = GitHubJob()
-while 1:
-    job_object.git_job()
-    time.sleep(1)
+job_object.git_job()
+# while 1:
+#     job_object.git_job()
+#     time.sleep(1)
